36c456faee54b78e609fce21fa58873a2f30e873
fazeplayYT <gabydecat257@gmail.com>
2025-02-09 18:00:03 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,10 +12,28 @@ self.addEventListener("install", event => {
             return cache.addAll(ASSETS);
         })
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.map(key => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        fetch(event.request).catch(() => caches.match("./offline.html"))
+        fetch(event.request).catch(() => {
+            return caches.match(event.request).then(response => {
+                return response || caches.match("./offline.html");
+            });
+        })
     );
 });
